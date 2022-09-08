f3301ab7a31fac385438f89ca0f54a7b27e8ad3c
CodeDragi <codedragi@gmail.com>
2022-09-08 16:14:33 +0200

    Initial commit

---
+++ frontend/src/components/Auth/AuthForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AuthForm = () => {
+    return(
+        <React.Fragment>
+            <h1>Se connecter</h1>
+                <form onSubmit={()=>{}}>
+                    <label htmlFor="email">Votre email</label>
+                    <input type="email" id="email" required/>
+
+                    <label htmlFor="password">Votre mot de passe</label>
+                    <input type="password" id="password" required/>
+
+                    <button type="button" onClick={()=>{}}>Se connecter</button>
+                </form>
+        </React.Fragment>
+    )
+    
+}
+
+export default AuthForm;
